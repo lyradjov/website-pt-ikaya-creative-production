3ce231ffb16aa613e081803bf7b47b79c7d5956f
jovdar <jruindungan1@gmail.com>
2025-02-24 22:39:25 +0800

    perbaikan Source code tambahkan scroll animation

--- script.js
+++ script.js
@@ -71,6 +71,26 @@ galeriNextButton.addEventListener("click", () => {
 
 galeriStartAutoplay();
 
+document.addEventListener('DOMContentLoaded', function() {
+  const leftElements = document.querySelectorAll('.fade-in-left');
+  const rightElements = document.querySelectorAll('.fade-in-right');
+  
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+          }
+      });
+  }, { threshold: 0.1 });
+
+  leftElements.forEach(element => {
+      observer.observe(element);
+  });
+
+  rightElements.forEach(element => {
+      observer.observe(element);
+  });
+});
 
 
 
